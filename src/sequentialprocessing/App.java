7a3b92070e13323ef11f0ed22fefd53d388b5a12
Israel Huerta <israelhf24@gmail.com>
2024-12-20 12:46:58 -0600

    feat: Replace Runner1 and Runner2 with WorkerThread implementing Runnable

--- src/sequentialprocessing/App.java
+++ src/sequentialprocessing/App.java
@@ -1,54 +1,19 @@
 package sequentialprocessing;
 
-class Runner1 extends Thread {
-    public void execute() {
-        for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Runner1: " + i);
-        }
-    }
-
+class WorkerThread implements Runnable {
     @Override
     public void run() {
-        execute();
-    }
-}
-
-class Runner2 extends Thread {
-    public void execute() {
         for (int i = 0; i < 10; i++) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Runner2: " + i);
+            System.out.println(i);
         }
-    }
-
-    @Override
-    public void run() {
-        execute();
     }
 }
 
 public class App {
     public static void main(String[] args) {
-        Thread t1 = new Runner1();
-        Thread t2 = new Runner2();
-        t1.start();
-        t2.start();
-        // we can wait for the thread to finish: join()
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("Finished the tasks");
+        Thread t = new Thread(new WorkerThread());
+        t.setPriority(Thread.MAX_PRIORITY);
+        t.start();
+        System.out.println("This is in the main thread...");
     }
 }
